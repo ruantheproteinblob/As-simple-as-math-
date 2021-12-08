4ded219f80b58f1fbccddb8bee7a88987c6af7e4
ruantheproteinblob <94187502+ruantheproteinblob@users.noreply.github.com>
2021-12-08 17:10:58 -0300

    Add files via upload

---
+++ VALOR_X.py
@@ -0,0 +1,15 @@
+print("Vamos encontrar o valor de X")
+
+print("Digite um valor para Y")
+Y = input()
+
+print("Digite um valor para B")
+B = input()
+
+print("Digite um valor para A")
+A = input()
+
+S = int(Y) - int(B) / int(A)
+
+print("O valor é")
+print(S)
---
+++ calculo3000.py
@@ -0,0 +1,14 @@
+print ('Aqui você poderá fazer cálculos de primeiro grau, PARA encontrar y apenas. Vamos começar?')
+
+print ('iNSIRA A')
+A= input()
+
+print ('iNSIRA B')
+B= input()
+
+print ('iNSIRA X')
+X= input()
+
+S = int(A)*int(X)+int(B)
+print ('O valor DE Y é')
+print (S)
